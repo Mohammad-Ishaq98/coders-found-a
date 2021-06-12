6576ae3dd26670c6910304b6968bf22748aaef2e
Mohammad-Ishaq98 <mdishaqibrohim@gmail.com>
2021-06-12 23:45:45 +0600

    first commit01977

---
+++ Resources/js/main.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+	var mixer = mixitup('.container');
+});
+
+
+function openNav(){
+	getElementById("myNav").style.width=100%;
+}
+
+
+function closeNav(){
+	getElementById("myNav").style.width=0%;
+}
